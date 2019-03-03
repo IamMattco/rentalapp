e657dfba0037cf70a923fd815327ddf69833ef03
dev.mroz@gmail.com <dev.mroz@gmail.com>
2019-03-03 14:40:20 +0000

    api wip

---
+++ app/models/products.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+//Define a schema
+const Schema = mongoose.Schema;
+
+const ProductSchema = new Schema({
+ name: {
+  type: String,
+  trim: true,  
+  required: true,
+ },
+ leased: {
+  type: Boolean,
+  trim: true,
+  required: true
+ }
+});
+module.exports = mongoose.model('Product', ProductSchema)
